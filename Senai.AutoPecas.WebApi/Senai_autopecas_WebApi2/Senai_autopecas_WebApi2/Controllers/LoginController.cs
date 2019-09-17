6e38a9711422da3ac7cd0be3d4899062e92c7144
regiamariana <48362221+regiamariana@users.noreply.github.com>
2019-09-17 17:04:09 -0300

    up

---
+++ Senai.AutoPecas.WebApi/Senai_autopecas_WebApi2/Senai_autopecas_WebApi2/Controllers/LoginController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Senai_autopecas_WebApi2.Domains;
+using Senai_autopecas_WebApi2.Interfaces;
+using Senai_autopecas_WebApi2.Repositories;
+using Senai_autopecas_WebApi2.ViewmModels;
+
+namespace Senai_autopecas_WebApi2.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IUsuarioRepository usuarioRepository { get; set; }
+
+        public LoginController()
+        {
+            usuarioRepository = new UsuarioRepository();
+
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel login)
+        {
+            try
+            {
+                Usuarios usuarioBuscado = usuarioRepository.BuscarPorEmailESenha(login);
+                if (usuarioBuscado == null)
+                    return NotFound(new { mensagem = "deu rui" });
+
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti, usuarioBuscado.Idusuario.ToString()),
+                    // é a permissão do usuário
+                    new Claim(ClaimTypes.Role, usuarioBuscado.Permissao),
+                };
+
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("AutoPecas-chave-autenticacao"));
+
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: "AutoPecas.WebApi",
+                    audience: "AutoPecas.WebApi",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new { mensagem = ex.Message });
+            }
+        }
+    }
+}
---
+++ Senai.AutoPecas.WebApi/Senai_autopecas_WebApi2/Senai_autopecas_WebApi2/Controllers/UsuarioController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai_autopecas_WebApi2.Domains;
+using Senai_autopecas_WebApi2.Interfaces;
+using Senai_autopecas_WebApi2.Repositories;
+
+namespace Senai_autopecas_WebApi2.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private IUsuarioRepository usuarioRepository { get; set; }
+
+        public UsuarioController()
+        {
+            usuarioRepository = new UsuarioRepository();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(usuarioRepository.Listar());
+        }
+
+        [Authorize]
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+
+            Usuarios usuario = usuarioRepository.BuscarPorId(id);
+            if (usuario == null)
+                return NotFound();
+            return Ok(usuario);
+
+        }
+
+        [Authorize(Roles = "ADMINISTRADOR")]
+        [HttpPost]
+        public IActionResult Cadastrar(Usuarios usuario)
+        {
+            try
+            {
+                usuarioRepository.Cadastrar(usuario);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new { mensagem = "Eita, erro: " + ex.Message });
+            }
+        }
+
+        [Authorize(Roles = "ADMINISTRADOR")]
+        [HttpPut]
+        public IActionResult Atualizar(Usuarios usuario)
+        {
+            Usuarios UsuarioBuscado = usuarioRepository.BuscarPorId(usuario.Idusuario);
+            if (UsuarioBuscado == null)
+                return NotFound();
+            usuarioRepository.Atualizar(usuario);
+            return Ok();
+        }
+
+        //[Authorize(Roles = "ADMINISTRADOR")]
+        //[HttpDelete("{id}")]
+        //public IActionResult Deletar(int id)
+        //{
+        //    usuarioRepository.Deletar(id);
+        //    return Ok();
+        //}
+    }
+}
